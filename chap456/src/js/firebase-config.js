aaf5aa243ce4b3c80330efdbf2ad1d2bd8c8d1da
reima4038 <reima4038@gmail.com>
2023-06-06 22:06:51 +0900

    chap4: firebaseの設定、webpackの設定をコミット

---
+++ chap456/src/js/firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDaDpbF8-nxcKedIZy6tr6pNLy8K4_a_pg",
+  authDomain: "social-login-chap456-f2c30.firebaseapp.com",
+  projectId: "social-login-chap456-f2c30",
+  storageBucket: "social-login-chap456-f2c30.appspot.com",
+  messagingSenderId: "839775187264",
+  appId: "1:839775187264:web:362e46d30b37a0b8900aa5"
+};
+export default firebaseConfig;
